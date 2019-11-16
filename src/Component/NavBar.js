874b5146299e8cd77f8d4d067315d13f05b26aa0
Meet1103 <meetmehta1103@gmail.com>
2019-11-16 02:22:53 -0700

    Added NavBar Component

---
+++ src/Component/NavBar.js
@@ -0,0 +1,35 @@
+import React , {Component} from 'react';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import "./NavBar.css";
+
+class NavBar extends Component {
+    state = {
+        value: 0,
+    }
+
+    handleChange = (event, newValue) => {
+        this.setState({value:newValue});
+    }
+
+    render() {
+        return (
+            <Tabs
+            value={this.state.value}
+            onChange={this.handleChange}
+            className="navbar"
+            indicatorColor="secondary"
+            textColor="primary"
+            centered
+            >
+                <Tab label="About Me" />
+                <Tab label="Experience" />
+                <Tab label="Portfolio" />
+                <Tab label="Skill" />
+                <Tab label="Contact Me" />
+            </Tabs>
+        )
+    }
+}
+
+export default NavBar
