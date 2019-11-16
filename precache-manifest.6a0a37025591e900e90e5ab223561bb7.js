6b9df47419966a63538e6a8aa4bd5914598967a4
Dhaval Mehta <mehtadhaval8228@gmail.com>
2019-11-16 15:22:02 +0530

    Updates

---
+++ precache-manifest.6a0a37025591e900e90e5ab223561bb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627907a999f5d8fc1940a29ca6f08b92",
+    "url": "/React-Portfolio-Template/index.html"
+  },
+  {
+    "revision": "7cbb343492be22c3a11e",
+    "url": "/React-Portfolio-Template/static/css/main.b044c59b.chunk.css"
+  },
+  {
+    "revision": "07e1e13bb1e78acf166b",
+    "url": "/React-Portfolio-Template/static/js/2.ba46c46e.chunk.js"
+  },
+  {
+    "revision": "7cbb343492be22c3a11e",
+    "url": "/React-Portfolio-Template/static/js/main.40fe643f.chunk.js"
+  },
+  {
+    "revision": "2d7d9d5df55ff3c1faab",
+    "url": "/React-Portfolio-Template/static/js/runtime-main.0bb88bc1.js"
+  }
+]);
