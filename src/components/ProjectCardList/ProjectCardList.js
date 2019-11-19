a593e97edc99b9d1216e77a29f3205d1cdb61ec7
Dhaval Mehta <mehtadhaval8228@gmail.com>
2019-11-19 13:41:46 +0530

    Minor fixes in the Cardlist component

--- src/components/ProjectCardList/ProjectCardList.js
+++ src/components/ProjectCardList/ProjectCardList.js
@@ -7,7 +7,7 @@ export default function ProjectCardList() {
   const project = {
     title: "Face Detector",
     des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    tags: ["React", "node.js", "Javascript"],
+    tags: ["React", "Node.js", "Javascript"],
     links: { launch: "#", github: "https://www.github.com" }
   }
 
@@ -16,7 +16,7 @@ export default function ProjectCardList() {
 
   return (
     <Paper style={{backgroundColor: "#f3f3f3"}}>
-      <Typography variant="h5" display="block" style={{ paddingLeft: 32 }}>
+      <Typography variant="h5" display="block" style={{ paddingLeft: 32, fontSize: 1.5rem }}>
         My Work
       </Typography>
       <Grid
