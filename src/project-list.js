a66295ce29b07ab6386f4e969ab0d3d9bb5c0bf4
rahulzura <rahuldesune@gmail.com>
2019-11-20 02:56:25 +0530

    fixed tags and description rendering

--- src/project-list.js
+++ src/project-list.js
@@ -3,7 +3,7 @@ let project_list = [
         image: "https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         title: "Project 1",
         des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        tags: ["HTML", "CSS", "Flexbox"],
+        tags: ["HTML", "CSS", "Flexbox", "React", "JavaScript"],
         links: {
             launch: "#",
             github: "https://www.github.com/Dhaval1403/React-Portfolio-Template"
@@ -23,7 +23,7 @@ let project_list = [
         image: "https://images.unsplash.com/photo-1553034545-32d4cd2168f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
         title: "Project 3",
         des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        tags: ["CSS", "Flexbox", "Grid"],
+        tags: ["CSS", "Flexbox", "Grid", "React", "JavaScript"],
         links: {
             launch: "#",
             github: "https://www.github.com/Dhaval1403/React-Portfolio-Template"
@@ -33,7 +33,7 @@ let project_list = [
         image: "https://images.unsplash.com/photo-1491975474562-1f4e30bc9468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
         title: "Project 4",
         des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        tags: ["React", "Redux", "Node.js"],
+        tags: ["React", "Redux", "Node.js", "HTML", "CSS", "JavaScript"],
         links: {
             launch: "#",
             github: "https://www.github.com/Dhaval1403/React-Portfolio-Template"
