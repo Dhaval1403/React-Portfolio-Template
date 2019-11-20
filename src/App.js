4f09fa6e4fee22ba4476a33bd38ecc7627cbebe3
faizhameed <faizftb@gmail.com>
2019-11-20 21:23:16 +0530

    added Work Experience Section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React , {Component} from 'react';
-import PersistentDrawerRight from "./components/Navigation/Navigation"
-import ProjectCard from './components/ProjectCard/ProjectCard';
-import './App.css';
+import React, { Component } from "react";
+import PersistentDrawerRight from "./components/Navigation/Navigation";
+import ProjectCard from "./components/ProjectCard/ProjectCard";
+import "./App.css";
+import Experience from "./components/Experience/experience.component";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <React.Fragment>
         <PersistentDrawerRight />
         <ProjectCard />
+        <Experience />
       </React.Fragment>
     );
   }
