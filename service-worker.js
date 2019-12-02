067ecfe7ef4aa4ef25827b5639375fdf06dc7963
Dhaval Mehta <mehtadhaval8228@gmail.com>
2019-12-02 10:32:36 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio-Template/precache-manifest.5daa11abd9ea943b637c9e3a751d51eb.js"
+  "/React-Portfolio-Template/precache-manifest.7469035b0913389fccaf36b2c0534a7f.js"
 );
 
 self.addEventListener('message', (event) => {
